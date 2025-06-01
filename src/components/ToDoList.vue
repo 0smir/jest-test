cf1934f288c1a417d8190cb488534c685d52baaa
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-06-01 19:53:08 +0300

    add todolist component with some items

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Lets Learn!</h1>
+    <span>list to learn</span>
+    <ul>
+      <li v-for="todo in toDos" key="todo.id" data-test="todo-item">
+        {{ todo.text }}
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDolist",
+  data() {
+    return {
+      toDos: [
+      {
+          id: 1,
+          text: "Learn Vue",
+          complited: false
+        },
+        {
+          id: 2,
+          text: "Learn Jest",
+          complited: false
+        },
+        {
+          id: 3,
+          text: "Learn how to combine Vue and Jest in one project",
+          complited: false
+        }
+      ]
+    }
+  }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
