e4c3361bdc125408da1b91df5ff26b1ff18af8b3
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-06-02 15:19:51 +0300

    add tests to check adding of a new to do item, add test to check is todo item is completed, optimize cod of tests with adding of beforeEach hook

--- tests/unit/ToDoList.spec.js
+++ tests/unit/ToDoList.spec.js
@@ -3,9 +3,13 @@ import ToDoList from "@/components/ToDoList.vue";
 
 
 describe("ToDoList.vue", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(ToDoList);
+  });
+
   it("should render toDo text", () => {
     const expected = /Learn\s+\w+/i;
-    const wrapper = mount(ToDoList);
     let todos = wrapper.findAll('[data-test="todo-item"]');
     //i have 3 started to do items so i need to iterate them
     todos.forEach(element => {
@@ -14,4 +18,19 @@ describe("ToDoList.vue", () => {
     // expect(todos[0].text()).toBe("Learn Vue") //one more way to write test, but only for first item
   });
 
+
+  it("should render new toDo item", async () => {
+    let currentItemLength = wrapper.findAll('[data-test="todo-item"]').length;
+    let expectedItemCount = currentItemLength + 1;
+
+    expect(wrapper.findAll('[data-test="todo-item"]')).toHaveLength(currentItemLength);
+    await wrapper.get('[data-test="new-todo-item"]').setValue('New todo');
+    await wrapper.get('[data-test="form"]').trigger('submit');
+    expect(wrapper.findAll('[data-test="todo-item"]')).toHaveLength(expectedItemCount);
+  });
+
+  it("should complite todo", async () => {
+    await wrapper.get('[data-test="todo-item-checkbox"]').setValue(true);
+    expect(wrapper.get('[data-test="todo-item"]').classes()).toContain('completed');
+  })
 })
