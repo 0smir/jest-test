9973b649cda6fb57b4abe63b721ebde943a7cce9
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-06-03 18:18:49 +0300

    add child and parent component, import child to parent

---
+++ src/components/Child.vue
@@ -0,0 +1,18 @@
+<template>
+  <div data-test="test-child">
+    <p>I'am Child component of: {{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChildComponent",
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Parent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Parent:</p>
+    <ChildComponent msg="Parent component"/>
+  </div>
+</template> 
+
+<script>
+import ChildComponent from '@/components/Child.vue';
+
+export default {
+  name: "ParentComponent",
+  components: {
+    ChildComponent
+  }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
