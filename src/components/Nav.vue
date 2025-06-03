dade54a0757838697ae1642cc6ec33293cceee0d
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-06-03 17:27:29 +0300

    add Navigation component with conditions of rendering 'My profile' link

---
+++ src/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav data-test="navigation">
+    <a id="home" href="#">Home</a>
+    <a id="about" href="#">About</a>
+    <a v-if="isLoggedIn" id="profile" href="#">My profile</a>
+  </nav>
+</template>
+
+<script>
+  export default {
+  name: "Navigation",
+  data() {
+    return {
+      isLoggedIn: false
+    }
+  }
+    
+  }
+</script>
+
+<style  scoped>
+nav {
+  width: 100%;
+}
+</style>
